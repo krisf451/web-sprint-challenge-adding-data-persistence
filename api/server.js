1fba5eddc6393cbdb819b46bbb6c817cee906a21
Kristian Fulkerson <kristianf451@gmail.com>
2021-11-04 18:31:34 -0700

    set up depencies, server, index, and first-migration

--- api/server.js
+++ api/server.js
@@ -1,1 +1,19 @@
-// build your server here and require it from index.js
+const express = require("express");
+
+const server = express();
+
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.send(`sanity check passed`);
+});
+
+server.use((err, req, res, next) => {
+  // eslint-disable-line
+  res.status(500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+module.exports = server;
